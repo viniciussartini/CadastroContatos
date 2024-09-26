9f69e032190471d7c81f0a95b0929a0a15dd5520
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-09-26 20:01:54 -0300

    Criação da página criar

--- CadastroContatos/Controllers/ContatoController.cs
+++ CadastroContatos/Controllers/ContatoController.cs
@@ -8,5 +8,20 @@ namespace CadastroContatos.Controllers
         {
             return View();
         }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
     }
 }
