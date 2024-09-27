bce5352df9d71deb60f1d2f424c54a8519c595d1
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-09-27 00:06:01 -0300

    Criação da tabela contatos no banco de dados SQL Server

--- CadastroContatos/Program.cs
+++ CadastroContatos/Program.cs
@@ -1,7 +1,15 @@
+using CadastroContatos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<BancoContext>(
+    item => item.UseSqlServer(configuration.GetConnectionString("DataBase")));
 
 var app = builder.Build();
 
